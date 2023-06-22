4789709a5ed46c7e82e7903eb0f16a11dfde1625
Gudkov <gudkov.id@mail.ru>
2023-06-22 19:01:03 +0300

    add motion heading

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,18 +9,40 @@ import { fadeIn } from "../variants.js";
 
 const Banner = () => {
   return (
-    <div className="section" id="home">
+    <div className="min-h-[85vh] lg:min-h-[78vh] flex items-center" id="home">
       <div className="container mx-auto">
-        <div>
-          <div className="mb-6 text-[36px] lg:text-[60px] font-semibold uppercase leading-[1]">
-            <span className="text-white">Я </span>
-            <TypeAnimation className="text-accent" sequence={['разработчик', 2000, 'junior-фронтендер', 2000, 'ищу работу 😅', 2000, ]} speed={50} wrapper='span' repeat={Infinity}/>
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center lg:text-left">
+            <div className="mb-6 text-[36px] lg:text-[60px] font-semibold uppercase leading-[1]">
+              <motion.h1
+                variants={fadeIn('up', 0.3)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{once: false, amount: 0.7}}
+                className="text-[50px] font-bold leading-[0.8] lg:text-[100px] mb-10"
+              >
+                ИЛЬЯ <span>ГУДКОВ</span>
+              </motion.h1>
+              <span className="text-white">Я </span>
+              <TypeAnimation className="text-accent" sequence={['разработчик', 2000, 'junior', 2000, 'фронтендер', 2000, 'ищу работу 😅', 2000, ]} speed={50} wrapper='span' repeat={Infinity}/>
+            </div>
+            <p className="mb-5"> Я начинающий разработчик, и я бы хотел стать частью коллектива.<br/>Я уверен, что могу внести свой вклад и стать достойным членом команды.<br/>Я верю, что работая вместе, мы можем превзойти все ожидания!🤟 </p>
+            <div className="mb-10">
+              <Link className="text-gradient text-[20px] btn-link  items-center self-start" to='work' smooth={true}>Моё портфолио</Link>
+            </div>
+            <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <a href="#" target="_blank" rel="noreferrer">
+                <FaGithub/>
+              </a>
+              <a href="#" target="_blank" rel="noreferrer">
+                <FaGithub/>
+              </a>
+              <a href="#" target="_blank" rel="noreferrer">
+                <FaGithub/>
+              </a>
+            </div>
           </div>
-          <p className="mb-5"> Я начинающий разработчик, и я бы хотел стать частью коллектива.<br/>Я уверен, что могу внести свой вклад и стать достойным членом команды.<br/>Я верю, что работая вместе, мы можем превзойти все ожидания!🤟 </p>
-          <div className="mb-10">
-            <Link className="text-gradient text-[20px] btn-link  items-center self-start" to='work' smooth={true}>Моё портфолио</Link>
-          </div>
-          <div>
+          <div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mx-auto">
             <img src={image} alt="me" />
           </div>
 
